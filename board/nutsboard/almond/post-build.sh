d28aa675502d6e795c3d51856a37d52937b86d10
Wig C <onlywig@gmail.com>
2017-01-04 09:57:10 +0800

    board: add nutsboard almond SoM support.

---
+++ board/nutsboard/almond/post-build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# post-build.sh for Almond SoM
+# 2017, Wig Cheng <onlywig@gmail.com>
+
+BOARD_DIR="$(dirname $0)"
+
+# copy the uEnv.txt to the output/images directory
+cp board/nutsboard/almond/uEnv.txt $BINARIES_DIR/uEnv.txt
+
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+  --rootpath "${TARGET_DIR}" \
+  --tmppath "${GENIMAGE_TMP}" \
+  --inputpath "${BINARIES_DIR}" \
+  --outputpath "${BINARIES_DIR}" \
+  --config "${GENIMAGE_CFG}"
+
+# copy the NAT relative functions to rootfs
+cp ${BOARD_DIR}/rootfs-additions/scripts/udhcpd_eth1.conf ${TARGET_DIR}/etc/udhcpd_eth1.conf
+cp ${BOARD_DIR}/rootfs-additions/scripts/eth1_nat.sh ${TARGET_DIR}/usr/bin/eth1_nat.sh
+
+# copy the jorjin wg7833 WiFi/Bluetooth firmware
+mkdir -p ${TARGET_DIR}/lib/firmware/ti-connectivity/
+cp ${BOARD_DIR}/rootfs-additions/firmware/ti-connectivity/wl18xx-conf.bin ${TARGET_DIR}/lib/firmware/ti-connectivity/wl18xx-conf.bin
+cp ${BOARD_DIR}/rootfs-additions/firmware/ti-connectivity/TIInit_11.8.32.bts ${TARGET_DIR}/lib/firmware/ti-connectivity/TIInit_11.8.32.bts
