2b348b14735c6e3e6bb9d03f605e8fe6394db9e1
Wig C <onlywig@gmail.com>
2017-01-21 21:20:41 +0800

    nutsboard_almond: upgrade the board support package to 4.4.32.

--- board/nutsboard/almond/post-build.sh
+++ board/nutsboard/almond/post-build.sh
@@ -12,18 +12,17 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 rm -rf "${GENIMAGE_TMP}"
 
+# copy the NAT relative functions to rootfs
+cp -rv ${BOARD_DIR}/rootfs-additions/scripts/udhcpd_eth1.conf ${TARGET_DIR}/etc/udhcpd_eth1.conf
+cp -rv ${BOARD_DIR}/rootfs-additions/scripts/eth1_nat.sh ${TARGET_DIR}/usr/bin/eth1_nat.sh
+
+# copy the jorjin wg7833 WiFi/Bluetooth firmware
+mkdir -p ${TARGET_DIR}/lib/firmware/ti-connectivity/
+cp -rv ${BOARD_DIR}/rootfs-additions/firmware/ti-connectivity ${TARGET_DIR}/lib/firmware/
+
 genimage \
   --rootpath "${TARGET_DIR}" \
   --tmppath "${GENIMAGE_TMP}" \
   --inputpath "${BINARIES_DIR}" \
   --outputpath "${BINARIES_DIR}" \
   --config "${GENIMAGE_CFG}"
-
-# copy the NAT relative functions to rootfs
-cp ${BOARD_DIR}/rootfs-additions/scripts/udhcpd_eth1.conf ${TARGET_DIR}/etc/udhcpd_eth1.conf
-cp ${BOARD_DIR}/rootfs-additions/scripts/eth1_nat.sh ${TARGET_DIR}/usr/bin/eth1_nat.sh
-
-# copy the jorjin wg7833 WiFi/Bluetooth firmware
-mkdir -p ${TARGET_DIR}/lib/firmware/ti-connectivity/
-cp ${BOARD_DIR}/rootfs-additions/firmware/ti-connectivity/wl18xx-conf.bin ${TARGET_DIR}/lib/firmware/ti-connectivity/wl18xx-conf.bin
-cp ${BOARD_DIR}/rootfs-additions/firmware/ti-connectivity/TIInit_11.8.32.bts ${TARGET_DIR}/lib/firmware/ti-connectivity/TIInit_11.8.32.bts
